5c758ce8c10ed79d12157f71cb53d427e5dada6b
boucara <traorebelco@gmail.com>
2018-02-28 23:19:14 +0100

    page modification bu

---
+++ client/src/app/modifilibrary/modifilibrary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modifilibrary',
+  templateUrl: './modifilibrary.component.html',
+  styleUrls: ['./modifilibrary.component.css']
+})
+export class ModifilibraryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
