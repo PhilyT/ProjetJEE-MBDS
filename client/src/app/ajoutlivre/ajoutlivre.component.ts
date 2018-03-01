72e72e6eabcbd4d827c50116466a33e167eb3bb8
temjanika-perkova <temjanika@gmail.com>
2018-03-01 01:16:28 +0100

    ajouter livre
    fonctionnel

--- client/src/app/ajoutlivre/ajoutlivre.component.ts
+++ client/src/app/ajoutlivre/ajoutlivre.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { LibrairyBooksService } from '../librairy-books.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-ajoutlivre',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ajoutlivre.component.css']
 })
 export class AjoutlivreComponent implements OnInit {
-
-  constructor() { }
+  nom: string;
+  releaseDate: string;
+  isbn: string;
+  author: string;
+  book: any;
+  idLibrairy: any;
+  constructor(private route: ActivatedRoute, private http: Http, private libraryBooksService: LibrairyBooksService) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.idLibrairy = params['idLibrairy'];
+    });
   }
 
+  ajoutBookToLibrary() {
+    this.book = { name: this.nom, releaseDate: this.releaseDate, isbn: this.isbn, author: this.author };
+    this.libraryBooksService.addBookToLibrary(this.idLibrairy, this.book).subscribe(response => console.log(response));
+  }
 }
